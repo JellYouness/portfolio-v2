a524337a79396adf78eecaa680d4a8f06bee4f74
JELLOULI Youness <younessjellouli12@gmail.com>
2024-04-22 23:00:19 +0100

    project gallery edited

--- utils/projectsGallery/stockGallery.tsx
+++ utils/projectsGallery/stockGallery.tsx
@@ -4,6 +4,6 @@ import Liste from '@/assets/projects/stock-manager/Liste.png';
 import login from '@/assets/projects/stock-manager/login.png'; // Assuming lowercase for consistency
 import ticket from '@/assets/projects/stock-manager/ticket.png';
 
-const stockGallery = [Caisse, Insert, Liste, login, ticket];
+const stockGallery = [Caisse, Insert, Liste, login];
 
 export default stockGallery;
