e40ae335ad35b273795c9f188d19b6682ad26e9d
Youness Jellouli <younessjellouli12@gmail.com>
2025-04-01 13:39:02 +0200

    Update Footer component to display the current year dynamically

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 const Footer = () => {
+    // get the current year
+    const currentYear = new Date().getFullYear();
     return (
-        <footer>
-            <div className="container mx-auto py-4">
-                <p className="text-center text-sm">
-                    © 2024 <span className="text-primary-main">Youness JELLOULI</span>. All rights reserved.
-                </p>
-            </div>
-        </footer>
+      <footer>
+        <div className="container mx-auto py-4">
+          <p className="text-center text-sm">
+            © {currentYear}{" "}
+            <span className="text-primary-main">Youness JELLOULI</span>. All
+            rights reserved.
+          </p>
+        </div>
+      </footer>
     );
 };
 
