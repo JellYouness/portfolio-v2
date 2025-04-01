c6581f4bedb77483eafca2d06ee08fff43880bca
Youness Jellouli <younessjellouli12@gmail.com>
2025-04-01 18:23:29 +0200

    Project Section: add filtering by services

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,12 +1,18 @@
 "use client";
 import { motion, useInView } from "framer-motion";
 import projects from "@/utils/projectsList";
+import { filterServices } from "@/utils/servicesList";
 import { GitHub, Language } from "@mui/icons-material";
 import Image from "next/image";
 import Link from "next/link";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SlideUp from "@/utils/SlideUp";
 import ProjectGallery from "./ProjectGallery";
+
+interface Service {
+  icon: string;
+  title: string;
+}
 
 const Projects = () => {
   const ref = useRef(null);
@@ -15,15 +21,29 @@ const Projects = () => {
     initial: { y: 50, opacity: 0 },
     animate: { y: 0, opacity: 1 },
   };
+  const [filteredProjects, setFilteredProjects] = useState(projects);
+  const [selectedServices, setSelectedServices] = useState<Service[]>([]);
 
-  const [isOpen, setOpen] = useState(false);
-  const onOpen = () => {
-    setOpen(true);
+  const handleServicesSelect = (service: Service) => {
+    if (selectedServices.includes(service)) {
+      setSelectedServices(selectedServices.filter((s) => s !== service));
+    } else {
+      setSelectedServices([...selectedServices, service]);
+    }
   };
 
-  const onClose = () => {
-    setOpen(false);
-  };
+  useEffect(() => {
+    if (selectedServices.length === 0) {
+      setFilteredProjects(projects);
+    } else {
+      const filtered = projects.filter((project) =>
+        project.chips.some((chip) =>
+          selectedServices.some((service) => chip.img === service.icon)
+        )
+      );
+      setFilteredProjects(filtered);
+    }
+  }, [selectedServices]);
 
   return (
     <section id="Projects" className="w-9/12 mx-auto pt-12 pb-10">
@@ -31,78 +51,116 @@ const Projects = () => {
         <h3 className="text-5xl py-10 font-semibold text-center">
           <span className="border-b-4 border-primary-main">My Work</span>
         </h3>
-        <div
-          ref={ref}
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8"
-        >
-          {projects.map((project, index) => (
-            <motion.div
-              variants={cardVariants}
-              initial="initial"
-              animate={isInView ? "animate" : "initial"}
-              transition={{ duration: 0.1, delay: index * 0.15 }}
-              key={index}
-              className="bg-gray-200 dark:bg-gray-800 rounded-lg shadow-lg p-4 relative hover:scale-[1.02] transition-all"
-              onClick={onOpen}
-            >
+        <div>
+          <p className="text-center text-lg mb-4">Filters:</p>
+          <div className="flex justify-center items-center gap-6 mb-4 flex-wrap">
+            {filterServices.map((service, index) => (
               <Image
-                src={project.image}
-                alt={project.title}
-                placeholder="blur"
+                key={index}
+                src={service.icon}
+                alt={service.title}
                 loading="lazy"
-                className="md:h-[170px] lg:h-[190px] 3xl:h-[245px] object-fill mb-4 rounded-lg border-2 border-gray-600 border-b-4"
+                className={`size-10 inline-block ${
+                  selectedServices.includes(service)
+                    ? "opacity-100"
+                    : "opacity-40"
+                } hover:opacity-100 hover:scale-110 transition-all cursor-pointer`}
+                onClick={() => {
+                  handleServicesSelect(service);
+                }}
               />
-              <h4 className="text-xl text-black dark:text-white font-semibold mb-2">
-                {project.title}
-              </h4>
-              <p className="text-gray-500 min-h-[150px] mb-4">{project.body}</p>
-              <div className="flex gap-3 md:gap-4 items-center flex-wrap absolute bottom-4 left-4">
-                {project.chips.map((chip, index) => (
-                  <Image
-                    key={index}
-                    src={chip.img}
-                    alt=""
-                    loading="lazy"
-                    className="w-9 hover:scale-110 transition-all"
-                  />
-                ))}
-              </div>
-
-              <div
-                className={
-                  project.images
-                    ? "flex gap-1 absolute top-6 right-12 text-white"
-                    : "flex gap-1 absolute top-6 right-6 text-white"
-                }
+            ))}
+          </div>
+          <div className="flex justify-center items-center gap-6 mb-8 flex-wrap">
+            {selectedServices.length > 0 && (
+              <button
+                className="bg-red-500 text-white text-sm px-2 py-1 rounded-lg hover:bg-red-600 transition-all"
+                onClick={() => setSelectedServices([])}
               >
-                {project.github && (
-                  <Link
-                    href={project.github}
-                    target="_blank"
-                    className="flex items-center justify-center p-2 bg-black rounded-full hover:scale-110 transition-all"
-                  >
-                    <GitHub className="text-2xl" />
-                  </Link>
-                )}
-                {project.preview && (
-                  <Link
-                    href={project.preview}
-                    target="_blank"
-                    className="flex items-center justify-center p-2 bg-black rounded-full hover:scale-110 transition-all"
-                  >
-                    <Language className="text-2xl" />
-                  </Link>
-                )}
-                {project.images && (
-                  <ProjectGallery
-                    title={project.title}
-                    images={project.images}
-                  />
-                )}
-              </div>
-            </motion.div>
-          ))}
+                Clear Filters
+              </button>
+            )}
+          </div>
         </div>
+        {filteredProjects.length > 0 ? (
+          <div
+            ref={ref}
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8"
+          >
+            {filteredProjects.map((project, index) => (
+              <motion.div
+                variants={cardVariants}
+                initial="initial"
+                animate={isInView ? "animate" : "initial"}
+                transition={{ duration: 0.1, delay: index * 0.15 }}
+                key={index}
+                className="bg-gray-200 dark:bg-gray-800 rounded-lg shadow-lg p-4 relative hover:scale-[1.02] transition-all"
+              >
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  placeholder="blur"
+                  loading="lazy"
+                  className="md:h-[170px] lg:h-[190px] 3xl:h-[245px] object-fill mb-4 rounded-lg border-2 border-gray-600 border-b-4"
+                />
+                <h4 className="text-xl text-black dark:text-white font-semibold mb-2">
+                  {project.title}
+                </h4>
+                <p className="text-gray-500 min-h-[150px] mb-4">
+                  {project.body}
+                </p>
+                <div className="flex gap-3 md:gap-4 items-center flex-wrap absolute bottom-4 left-4">
+                  {project.chips.map((chip, index) => (
+                    <Image
+                      key={index}
+                      src={chip.img}
+                      alt=""
+                      loading="lazy"
+                      className="w-9 hover:scale-110 transition-all"
+                    />
+                  ))}
+                </div>
+
+                <div
+                  className={
+                    project.images
+                      ? "flex gap-1 absolute top-6 right-12 text-white"
+                      : "flex gap-1 absolute top-6 right-6 text-white"
+                  }
+                >
+                  {project.github && (
+                    <Link
+                      href={project.github}
+                      target="_blank"
+                      className="flex items-center justify-center p-2 bg-black rounded-full hover:scale-110 transition-all"
+                    >
+                      <GitHub className="text-2xl" />
+                    </Link>
+                  )}
+                  {project.preview && (
+                    <Link
+                      href={project.preview}
+                      target="_blank"
+                      className="flex items-center justify-center p-2 bg-black rounded-full hover:scale-110 transition-all"
+                    >
+                      <Language className="text-2xl" />
+                    </Link>
+                  )}
+                  {project.images && (
+                    <ProjectGallery
+                      title={project.title}
+                      images={project.images}
+                    />
+                  )}
+                </div>
+              </motion.div>
+            ))}
+          </div>
+        ) : (
+          <div className="col-span-3 text-center text-xl mt-8">
+            No projects found for the selected filters.
+          </div>
+        )}
       </SlideUp>
     </section>
   );
--- utils/servicesList.ts
+++ utils/servicesList.ts
@@ -1,24 +1,24 @@
-import bootstrap from '@/public/images/bootstrap.svg';
-import css from '@/public/images/css.svg';
-import figma from '@/public/images/figma.svg';
-import github from '@/public/images/github.svg';
-import html from '@/public/images/html.svg';
-import js from '@/public/images/js.svg';
-import laravel from '@/public/images/laravel.svg';
-import mysql from '@/public/images/mysql.svg';
-import php from '@/public/images/php.svg';
-import tailwind from '@/public/images/tailwind.svg';
-import vscode from '@/public/images/vscode.svg';
-import react from '@/public/images/react.svg';
-import mui from '@/public/images/mui.svg';
-import nextjs from '@/public/images/nextjs-icon.svg';
-import mongodb from '@/public/images/mongodb.svg';
-import prisma from '@/public/images/prisma.svg';
-import supabase from '@/public/images/supabase.png';
-import ts from '@/public/images/Typescript.svg';
-import redux from '@/public/images/redux.svg';
-import vercel from '@/public/images/vercel.svg';
-import framerMotion from '@/public/images/framer-motion.svg';
+import bootstrap from "@/public/images/bootstrap.svg";
+import css from "@/public/images/css.svg";
+import figma from "@/public/images/figma.svg";
+import github from "@/public/images/github.svg";
+import html from "@/public/images/html.svg";
+import js from "@/public/images/js.svg";
+import laravel from "@/public/images/laravel.svg";
+import mysql from "@/public/images/mysql.svg";
+import php from "@/public/images/php.svg";
+import tailwind from "@/public/images/tailwind.svg";
+import vscode from "@/public/images/vscode.svg";
+import react from "@/public/images/react.svg";
+import mui from "@/public/images/mui.svg";
+import nextjs from "@/public/images/nextjs-icon.svg";
+import mongodb from "@/public/images/mongodb.svg";
+import prisma from "@/public/images/prisma.svg";
+import supabase from "@/public/images/supabase.png";
+import ts from "@/public/images/Typescript.svg";
+import redux from "@/public/images/redux.svg";
+import vercel from "@/public/images/vercel.svg";
+import framerMotion from "@/public/images/framer-motion.svg";
 
 export const services = [
   {
@@ -95,18 +95,23 @@ export const services = [
   },
 ];
 
+const unusedServices = ["Github", "Figma", "VS Code", "Vercel"];
+
+export const filterServices = services.filter(
+  (service) => !unusedServices.includes(service.title)
+);
+
 export const servicesSoon = [
-    
-    {
-        icon: mongodb,
-        title: 'MongoDB'
-    },
-    {
-        icon: supabase,
-        title: 'Supabase'
-    },
-    {
-        icon: prisma,
-        title: 'Prisma'
-    }
+  {
+    icon: mongodb,
+    title: "MongoDB",
+  },
+  {
+    icon: supabase,
+    title: "Supabase",
+  },
+  {
+    icon: prisma,
+    title: "Prisma",
+  },
 ];
