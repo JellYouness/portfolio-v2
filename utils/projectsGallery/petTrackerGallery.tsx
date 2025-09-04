bda6bd14a5c41169514dfac703016115d6153dbd
Youness JELLOULI <younessjellouli12@gmail.com>
2025-09-04 01:18:03 +0100

    Add new images for Pet Tracker project and update import paths in petTrackerGallery.tsx

--- utils/projectsGallery/petTrackerGallery.tsx
+++ utils/projectsGallery/petTrackerGallery.tsx
@@ -1,16 +1,16 @@
-import Home from '@/assets/projects/pet-tracker/home.png';
-import MyAnimals from '@/assets/projects/pet-tracker/myanimals.png';
-import Search from '@/assets/projects/pet-tracker/search.png';
-import Animal from '@/assets/projects/pet-tracker/animal.png';
-import Animal1 from '@/assets/projects/pet-tracker/animal1.png';
-import Animal2 from '@/assets/projects/pet-tracker/animal2.png';
-import Photos from '@/assets/projects/pet-tracker/photos.png';
-import Medic from '@/assets/projects/pet-tracker/medic.png';
-import Med from '@/assets/projects/pet-tracker/med.png';
-import Perdu from '@/assets/projects/pet-tracker/perdu.png';
-import MarkPerdu from '@/assets/projects/pet-tracker/mark-perdu.png';
-import Profile from '@/assets/projects/pet-tracker/profile.png';
-import Profile2 from '@/assets/projects/pet-tracker/profile2.png';
+import Home from '@/assets/pet-tracker/home.png';
+import MyAnimals from '@/assets/pet-tracker/myanimals.png';
+import Search from '@/assets/pet-tracker/search.png';
+import Animal from '@/assets/pet-tracker/animal.png';
+import Animal1 from '@/assets/pet-tracker/animal1.png';
+import Animal2 from '@/assets/pet-tracker/animal2.png';
+import Photos from '@/assets/pet-tracker/photos.png';
+import Medic from '@/assets/pet-tracker/medic.png';
+import Med from '@/assets/pet-tracker/med.png';
+import Perdu from '@/assets/pet-tracker/perdu.png';
+import MarkPerdu from '@/assets/pet-tracker/mark-perdu.png';
+import Profile from '@/assets/pet-tracker/profile.png';
+import Profile2 from '@/assets/pet-tracker/profile2.png';
 
 const stockGallery = [Home, MyAnimals, Search, Animal, Animal1, Animal2, Photos, Medic, Med, Perdu, MarkPerdu, Profile, Profile2];
 
